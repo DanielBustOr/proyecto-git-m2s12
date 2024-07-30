e808ab775cb5fd0fe7a4271391c9eea3fb52d70a
Daniel <dbustosorellana@gmail.com>
2024-07-29 23:23:39 -0400

    agregando javascript al proyecto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
 /* Este es un comentario JS*/
 
-alert("Hola Mundo Git!!!")
+alert("Hola Mundo Git1!!!")
+
+console.log("Hola Mundo Git2!!!")
+
+alert("Hola Mundo Git4!!!")
+
+alert("Hola Mundo Git4!!!")
+
